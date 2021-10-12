2fb29909a2bfb2fac441680373b395be83a4abac
NK <navaneethk002@gmail.com>
2021-10-12 13:42:45 -0400

    Send a text email using smtp and gmail

--- main.py
+++ main.py
@@ -42,11 +42,18 @@ print(new.set)
 
 
 port = 465  # For SSL
-password = input("****")
+password = input("Type your password and press enter: ")
+smtp_server = "smtp.gmail.com"
+sender_email = "morning.newsletter.tech@gmail.com"
+receiver_email = "krishnanaveen858@gmail.com"
+message = """\
+Subject: Hi there
 
+This message is sent from Python."""
 # Create a secure SSL context
 context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login("morning.newsletter.tech@gmail.com", password)
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
 
