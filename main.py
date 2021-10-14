c11693579c2d84579a2b4c3830085288999a657a
NK <navaneethk002@gmail.com>
2021-10-14 11:52:39 -0400

    added todo

--- main.py
+++ main.py
@@ -65,3 +65,7 @@ with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(sender_email, password)
     server.sendmail(sender_email, receiver_email, message)
 
+
+#TODO: send html emial
+#TODO: different websites
+
